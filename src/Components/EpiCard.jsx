b5bb3e079ac0a526dd1261d374b81a6d6b5aef93
Briss <alejandra@gmail.com>
2023-07-10 17:48:58 -0600

    mas

--- src/Components/EpiCard.jsx
+++ src/Components/EpiCard.jsx
@@ -10,9 +10,12 @@ const EpiCard = ({ title, image, summary, season, number }) => {
               <img src={image} alt={title} style={{ objectFit: 'cover', maxHeight: '300px' }} />
 
               <div className='card-body'>
-                <h1 className='text-truncate'>Episodio {number} <h1 style={{ color: 'red' }}>{title}</h1></h1>
-                <h2>Temporada{season}</h2>
-                <p dangerouslySetInnerHTML={{ __html: [summary] }} />
+                <h1 style={{ fontSize: '2vw' }} className='text-truncate'>Episodio {number}
+                  <h1 style={{ color: 'red', fontSize: '1.5vw' }}>{title}</h1>
+                  <h2 style={{ fontSize: '1.5vw' }}>Temporada{season}</h2>
+                </h1>
+
+                <p style={{ fontFamily: 'Gill Sans' }} dangerouslySetInnerHTML={{ __html: [summary] }} />
                 <div className='d-flex justify-content-between align-items-center' />
               </div>
             </div>

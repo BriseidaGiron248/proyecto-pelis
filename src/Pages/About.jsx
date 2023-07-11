66bbb9cac586de0099f89536a9ca999eb05230b6
Briss <alejandra@gmail.com>
2023-07-10 18:48:21 -0600

    barranav para about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom'
 import SectioOne from '../Components/SectioOne'
 import SectionTwo from '../Components/SectionTwo'
 import SectionThree from '../Components/SectionThree'
+
 const About = () => {
   const [peli, setPeli] = useState({})
 
@@ -23,10 +24,34 @@ const About = () => {
   return (
 
     <>
+      <>
+        <nav id='navbar-example2' className='navbar navbar-light bg-light'>
+          <a className='navbar-brand' href='#' />
+          <ul className='nav nav-pills'>
+            <li className='nav-item'>
+              <a className='nav-link' href='#Resumen'>
+                Resumen
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#Episodios'>
+                Episodios
+              </a>
+            </li>
+            <li className='nav-item'>
+              <a className='nav-link' href='#Reparto'>
+                Reparto
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+      </>
 
       <main className='angled-gradient'>
         <div>
-          <div>
+
+          <div id='Resumen'>
             <h1 style={{ background: 'red', maxWidth: '100%', maxHeight: '300px' }} id='titulo'>Los mejores Shows  TvMaze</h1>
             <SectioOne
               key={peli.id}
@@ -34,16 +59,45 @@ const About = () => {
               summary={peli.summary}
               image={peli.image?.original}
             />
+            <footer className='text-muted py-5'>
+              <div className='container'>
+                <p className='float-end mb-1'>
+                  <a href='#'>Volver Arriba</a>
+                </p>
+              </div>
+            </footer>
           </div>
-          <div>
+
+          <div id='Episodios'>
+            <h1 style={{ background: 'red', maxWidth: '100%', maxHeight: '150px' }} id='titulo'>Episodios</h1>
             <SectionTwo
               peliId={peli.id}
             />
+            <footer className='text-muted py-5'>
+              <div className='container'>
+                <p className='float-end mb-1'>
+                  <a href='#'>Volver Arriba</a>
+                </p>
+              </div>
+            </footer>
+          </div>
+
+          <div id='Reparto'>
+
+            <h1 style={{ background: 'red', maxWidth: '100%', maxHeight: '150px' }} id='titulo'>Reparto</h1>
             <SectionThree
               peliId={peli.id}
             />
+            <footer className='text-muted py-5'>
+              <div className='container'>
+                <p className='float-end mb-1'>
+                  <a href='#'>Volver Arriba</a>
+                </p>
+              </div>
+            </footer>
 
           </div>
+
         </div>
       </main>
 
